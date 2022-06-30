06b3ca7e5b759919481cf5d956847d234855ce41
EasyPage <74344815+easypage@users.noreply.github.com>
2022-06-30 13:29:09 +0900

    Update calendar.js

--- router/calendar.js
+++ router/calendar.js
@@ -14,6 +14,7 @@ router.post("/", async function (req, res) {
     return res.status(400).send({ message: "failed: request does not exist" });
   } else {
     console.log("있습니다.");
+    console.log(req.body);
     return res.status(200).send({
       message: "successfully",
     });
