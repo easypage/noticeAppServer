ba1f6a3b5aa79f8f9b72507d17eeb7d3aa934ff7
EasyPage <74344815+easypage@users.noreply.github.com>
2022-07-07 15:35:45 +0900

    Update calenderModel.js

--- Entity/calenderModel.js
+++ Entity/calenderModel.js
@@ -2,7 +2,13 @@ const { default: mongoose } = require("mongoose");
 
 const calenderSchema = new mongoose.Schema({
   name: { type: "string", required: true },
-  state: String,
+  state: {
+    type: "String",
+    enum: {
+      values: ["지각", "결석", "조퇴"],
+      message: "{VALUE} 잘못된 값입니다.",
+    },
+  },
   reason: String,
   privateReason: Boolean,
   date: String,
